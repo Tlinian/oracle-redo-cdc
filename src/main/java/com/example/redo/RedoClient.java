082b5cacd9d4b145c0ce56066472aa90ab8b77bd
xdream <2629731238@qq.com>
2025-12-15 23:40:45 +0800

    redo client

--- src/main/java/com/example/redo/RedoClient.java
+++ src/main/java/com/example/redo/RedoClient.java
@@ -11,12 +11,17 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class RedoClient {
     private Config config;
     private MetadataManager  metadataManager;
     private ArrayBlockingQueue<RedoEvent> redoEventList;
     private RecordDeserializer recordDeserializer;
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+
     public RedoClient(Config config) {
         this.config = config;
         this.metadataManager = new MetadataManager(config);
@@ -30,8 +35,22 @@ public class RedoClient {
 
     public void start() {
         String redoFileName = config.getRedoFileName();
-        RedoMiner redoMiner = new RedoMiner(config, recordDeserializer,metadataManager.getChecker());
-        redoMiner.parseRedoFile();
+        executorService.submit(() ->{
+            RedoMiner redoMiner = new RedoMiner(config, recordDeserializer,metadataManager.getChecker());
+            redoMiner.parseRedoFile();
+        });
+    }
+
+    public void stop() {
+        executorService.shutdownNow();
+    }
+
+    public RedoEvent redoEvent() {
+        try {
+            return redoEventList.poll(3000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public RedoEvent getRedoEvent() {
---
+++ src/test/java/Demo.java
@@ -0,0 +1,23 @@
+import com.example.redo.RedoClient;
+import com.example.redo.config.Config;
+import com.example.redo.deserialize.RedoEvent;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Demo {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        RedoClient redoClient = new RedoClient(new Config(Path.of("D:\\AI-project\\code2\\oracle-redo-analysis\\src\\main\\resources\\application.properties")));
+        redoClient.init();
+        redoClient.start();
+        while(true){
+            RedoEvent redoEvent = redoClient.redoEvent();
+            if (redoEvent != null){
+                System.out.println(redoEvent);
+            }else  {
+                Thread.sleep(3000);
+                System.out.println("no redo event");
+            }
+        }
+    }
+}
